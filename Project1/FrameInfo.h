861fa9794ff21eb01d8aea7fb5a5a3bd4359cd41
cbcbc82 <cbcbc82@daum.net>
2024-03-22 11:46:21 +0900

    frameinfo 추가
    각frame이 사용할 resource들을 한곳에 모아둠

---
+++ Project1/FrameInfo.h
@@ -0,0 +1,15 @@
+#pragma once
+#include "Camera.h"
+
+#include <vulkan/vulkan.h>
+
+namespace jhb {
+	struct FrameInfo
+	{
+		int frameIndex;
+		float frameTime;
+		VkCommandBuffer commandBuffer;
+		Camera& camera;
+	};
+}
+
